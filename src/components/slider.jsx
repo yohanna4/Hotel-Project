f455d9c6390fec9b5b9e9d0241464c211b3c74d0
Yohanna Yimer <61965379+yohanna4@users.noreply.github.com>
2023-12-19 13:59:26 +0300

    Update slider.jsx

--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -28,7 +28,7 @@ export default class Slider  extends Component {
               <div class="carousel-item"  interval={200}>
                 <img class="d-block w-100" src={pic2} alt="Second slide"/>
                 <div class="carousel-caption d-none d-md-block">
-                    <h5>Welcome to</h5><h5> Kenenisa Hotel Plus</h5>
+                    <h5>Welcome to</h5><h5> Omega Hotel Plus</h5>
                 </div>
               </div>
               <div class="carousel-item" interval={200}>
